a1734f3cc6041f8dd153e9d47e25573f6edb5c29
itzzmeivan <charlesivangenese11@gmail.com>
2026-09-18 17:03:04 +0800

    Update all Files and Images

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function defineProperty() {
   osccred.style.right = "0";
   osccred.style.fontSize = "10px";
   osccred.style.fontWeight = "Bold";
-  osccred.style.color = "#black";
+  osccred.style.color = "#ffffff";
   osccred.style.fontFamily = "sans-serif";
   osccred.style.padding = "5px";
-  osccred.style.background = "#fff";
+  osccred.style.background = " #7a0000";
   osccred.style.borderTopLeftRadius = "5px";
   osccred.style.borderBottomRightRadius = "5px";
-  osccred.style.boxShadow = "0 0 5px #ccc";
+  osccred.style.boxShadow = "0 0 5px  #7a0000";
   document.body.appendChild(osccred);
 }
 
